2bc4281e1771ba9e4bd1ac0e3fc9da4b8829fe7b
Levente Bagi <leventeb@square-enix.com>
2014-02-03 14:37:07 +0000

    Fixed for international characters
    Fixes error: Encoding::CompatibilityError: incompatible character encodings: ASCII-8BIT and UTF-8

--- spec/phpass-ruby_spec.rb
+++ spec/phpass-ruby_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "PhpassRuby" do
@@ -13,6 +14,19 @@ describe "PhpassRuby" do
     known = '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0'
     @ps.check(@correct, known).should be_true
     @ps.check(@wrong, known).should be_false
+  end
+
+  it 'should work with international characters' do
+    hash = '$P$BqrxgWbGuXvBN11Eip1u1KTdtqcrbe.'
+      # took this hash from the PHP implementation:
+      #   http://www.openwall.com/phpass/
+      #   http://www.openwall.com/phpass/phpass-0.3.tar.gz
+    wrong_hash = '$P$B/x0n43EbvnfKB5YOFhetILL0FmhVg1'
+    correct = 'piña'
+    wrong = 'wrong'
+    @ps.check(correct, hash).should be_true
+    @ps.check(correct, wrong_hash).should be_false
+    @ps.check(wrong, hash).should be_false
   end
 end
 
